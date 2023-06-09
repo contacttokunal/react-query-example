d9ca7701e6ad861ddc35aa011de7e46d70433803
kunal <contacttokunal@gmail.com>
2023-06-10 01:14:41 +0530

    Managing Application State with zusland

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import AuthProvider from "./state-management/Auth/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import { TasksProvider } from "./state-management/tasks";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <AuthProvider>
       <TasksProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
