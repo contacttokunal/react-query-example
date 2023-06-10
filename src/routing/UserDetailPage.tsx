b4bf7854f858ba9ac562aec3be259586db1dfb10
kunal <contacttokunal@gmail.com>
2023-06-11 01:58:37 +0530

    Getting current data about current routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,11 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [serchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
